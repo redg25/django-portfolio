e47cbcab3dda3b565cfdf1c052595c00563ed75d
redg25 <regiscorblin2yahoo.fr>
2021-10-15 20:56:41 +0200

    Style project

--- projects/views.py
+++ projects/views.py
@@ -6,3 +6,8 @@ def all_projects(request):
     #query to return all project objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects':projects})
+
+def project_detail(request, pk):
+    #query to return all project objects
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project':project})
