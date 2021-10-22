911c9ea05a0f349b5692cfde258d4e38eef1d57f
redg25 <regiscorblin2yahoo.fr>
2021-10-22 11:44:59 +0200

    Finished tuto

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from projects import views
 
 app_name = "projects"
 urlpatterns = [
-    path('',views.all_projects),
+    path('',views.all_projects, name='all_projects'),
     path('<int:pk>',views.project_detail, name="project_detail")
 ]
